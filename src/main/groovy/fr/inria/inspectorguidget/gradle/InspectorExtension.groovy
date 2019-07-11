0f97d5857972823a7d4ee88eec7a812c2cd94978
arnobl <1052622+arnobl@users.noreply.github.com>
2019-07-11 11:33:03 +0200

    initial project

---
+++ src/main/groovy/fr/inria/inspectorguidget/gradle/InspectorExtension.groovy
@@ -0,0 +1,5 @@
+package fr.inria.inspectorguidget.gradle
+
+class InspectorExtension {
+    /** Options **/
+}
---
+++ src/main/groovy/fr/inria/inspectorguidget/gradle/InspectorPlugin.groovy
@@ -0,0 +1,14 @@
+package fr.inria.inspectorguidget.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Gradle plugin for InspectorGuidget.
+ */
+class InspectorPlugin implements Plugin<Project> {
+  @Override
+  void apply(final Project project) {
+
+  }
+}
