b2440f97750fbde602089402360df0fc00312876
Ludovic Richoux <ludovic.richoux@inria.fr>
2019-07-22 17:11:02 +0200

    feat(Inspectorplugin) building plugin structure

--- src/main/groovy/fr/inria/inspectorguidget/gradle/InspectorPlugin.groovy
+++ src/main/groovy/fr/inria/inspectorguidget/gradle/InspectorPlugin.groovy
@@ -1,7 +1,14 @@
 package fr.inria.inspectorguidget.gradle
 
+import com.beust.klaxon.Klaxon
+import fr.inria.inspectorguidget.api.analyser.UIDataAnalyser
+import fr.inria.inspectorguidget.data.UIData
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.logging.LogLevel
+import org.gradle.api.logging.Logger
+
 
 /**
  * Gradle plugin for InspectorGuidget.
@@ -10,5 +17,32 @@ class InspectorPlugin implements Plugin<Project> {
   @Override
   void apply(final Project project) {
 
+    Logger logger = project.getLogger()
+    PrintWriter pw = null
+    UIDataAnalyser analyser = new UIDataAnalyser()
+
+    logger.info("Adding input ressource...")
+    analyser.addInputResource(project.getProjectDir().getPath())
+
+    logger.info("adding dependencies path...")
+    Configuration configuration = project.getConfigurations().getByName("compile")
+    String[] dependencies = configuration.getDependencies().toArray(String[])
+    analyser.setSourceClasspath(dependencies)
+
+    logger.info("Extracting data...")
+    UIData data = analyser.extractUIData()
+
+    logger.info("Building data file...")
+    try {
+      pw = new PrintWriter("data.json")  // add parameter to change fileName
+      pw.print(new Klaxon().toJsonString(data,null))
+    } catch (FileNotFoundException e) {
+      logger.log(LogLevel.ERROR," ", e.fillInStackTrace())
+
+    } finally {
+      if(pw != null) {
+        pw.close()
+      }
+    }
   }
 }
