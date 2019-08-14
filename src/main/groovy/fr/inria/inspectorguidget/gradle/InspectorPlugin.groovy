2c70bd44546998796082a13023c329a8a9798285
Ludovic Richoux <ludovic.richoux@inria.fr>
2019-08-14 09:51:57 +0200

    new(License) Adding license headers

--- src/main/groovy/fr/inria/inspectorguidget/gradle/InspectorPlugin.groovy
+++ src/main/groovy/fr/inria/inspectorguidget/gradle/InspectorPlugin.groovy
@@ -1,17 +1,26 @@
+/*
+ * This file is part of InspectorGuidget.
+ * InspectorGuidget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * InspectorGuidget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with InspectorGuidget.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package fr.inria.inspectorguidget.gradle
 
 import fr.inria.inspectorguidget.api.analyser.UIDataAnalyser
 import fr.inria.inspectorguidget.data.UIData
 
 import com.beust.klaxon.Klaxon
-import org.apache.log4j.lf5.LogLevel
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Configuration
-import org.gradle.api.logging.Logger
-import org.gradle.internal.impldep.org.testng.collections.Lists
 
-import javax.swing.JList
 
 /**
  * Gradle plugin for InspectorGuidget.
